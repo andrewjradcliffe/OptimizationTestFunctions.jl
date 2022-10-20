4826a73b4b7dde0b8e974f9a5abfe17bc8366b05
Andrew Radcliffe <andrew.radcliffe@intel.com>
2022-10-20 13:31:59 -0700

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/OptimizationTestFunctions.jl
@@ -0,0 +1,5 @@
+module OptimizationTestFunctions
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using OptimizationTestFunctions
+using Test
+
+@testset "OptimizationTestFunctions.jl" begin
+    # Write your tests here.
+end
